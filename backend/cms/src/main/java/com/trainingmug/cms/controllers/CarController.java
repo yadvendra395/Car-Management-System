b205d9bfcc92bc02733269a0c693b8e497afe79f
yadvendra <yadvendra395@gmail.com>
2025-02-11 10:51:22 +0530

    MESSAGE

--- backend/cms/src/main/java/com/trainingmug/cms/controllers/CarController.java
+++ backend/cms/src/main/java/com/trainingmug/cms/controllers/CarController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:5173") // For React frontend
+
 @RestController
 @RequestMapping("/api/cars")
 public class CarController {
